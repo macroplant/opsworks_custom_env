1f42d4c82604ae4e6578720a60c2a612c13bba64
Alex Chumak <alex.chumak@gmail.com>
2016-04-25 20:35:35 -0400

    By default application's custom environment is an empty Hash

--- recipes/write_config.rb
+++ recipes/write_config.rb
@@ -10,6 +10,6 @@ node[:deploy].each do |application, deploy|
   custom_env_template do
     application application
     deploy deploy
-    env node[:custom_env][application]
+    env node[:custom_env][application] || {}
   end
 end
