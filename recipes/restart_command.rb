3cb91820808ea89110002500403022093fa2886f
chrisdevor <chrisdevor@macroplant.com>
2018-10-03 23:01:20 -0400

    ..

--- recipes/restart_command.rb
+++ recipes/restart_command.rb
@@ -1,10 +1,12 @@
 node[:deploy].each do |application, deploy|
   execute "restart Rails app #{application} for custom env" do
     cwd deploy[:current_path]
-    if node[:opsworks][:rails_stack] && node[:opsworks][:rails_stack][:name].eql? "apache_passenger"
-      command "touch #{deploy[:deploy_to]}/current/tmp/restart.txt"
-    elsif node[:opsworks][:rails_stack] && node[:opsworks][:rails_stack][:recipe].eql? "nginx_unicorn"
-      command "#{deploy[:deploy_to]}/shared/scripts/unicorn clean-restart"
+    if node[:opsworks][:rails_stack]
+      if node[:opsworks][:rails_stack][:name].eql? "apache_passenger"
+        command "touch #{deploy[:deploy_to]}/current/tmp/restart.txt"
+      elsif node[:opsworks][:rails_stack][:recipe].eql? "nginx_unicorn"
+        command "#{deploy[:deploy_to]}/shared/scripts/unicorn clean-restart"
+      end
     end
     user deploy[:user]
 
